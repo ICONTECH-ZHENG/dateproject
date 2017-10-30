c4fc040e8bc1d8335d3c974539a70e2ca0d88638
Shirong Zheng <shirongzheng1027@gmail.com>
2017-10-30 16:01:26 -0400

    add if-else statement

--- dateapp/routes.py
+++ dateapp/routes.py
@@ -7,16 +7,28 @@ from dateapp import app
 
 @app.route('/', methods=['GET'])
 def index():
-
-    TitleName = "Day of the Week Calculator"
-    HeadName = "Day of the Week Calculator"
-
-    userinput = request.args.get('userdate',' ')
-    
-    if userinput:
-       inputdate = str(userinput)
-       
-       return render_template("index.html", TitleName = TitleName, HeadName = HeadName, inputdate = inputdate)
-
-    else:
-       return render_template("index.html", TitleName = TitleName, HeadName = HeadName)
+    input_date = None
+    input_dat= None
+    if request.args:
+        input_date =request.args.get('datetime')
+    if input_dat:
+        day = str(input_date)
+        month, day, year = (int(x) for x in day.split('/'))
+        output_day = datetime.date(year, month, day).weekday()
+        if output_day == 0:
+            output_day="Monday"
+        elif output_day == 1:
+            output_day="Tuesday"
+        elif output_day == 2:
+            output_day="Wednesday"
+        elif output_day == 3:
+            output_day="Thursday"
+        elif output_day == 4:
+            output_day="Friday"
+        elif output_day == 5:
+            output_day="Saturday"
+        else:
+            output_day="Sunday"
+        
+     
+            
